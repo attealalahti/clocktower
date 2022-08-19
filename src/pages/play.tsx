5f0e88e652aa74276dfbd2dfad89cebe4f39751f
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-19 04:35:50 +0300

    Send a role to user through socket.io in pages/play.

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -2,14 +2,19 @@ import { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import { Character } from "../util/characters";
 
 const socket = io();
 
 const Play: NextPage = () => {
   const [connected, setConnected] = useState<boolean>(true);
+  const [role, setRole] = useState<Character | undefined>(undefined);
+
   useEffect(() => {
     socket.on("connect", () => setConnected(true));
     socket.on("disconnect", () => setConnected(false));
+    socket.on("role", (role: Character) => setRole(role));
+    socket.emit("ready");
     return () => {
       socket.off("connect");
       socket.off("disconnect");
@@ -28,6 +33,7 @@ const Play: NextPage = () => {
       </Head>
       <main>
         <p>{connected ? "Connected" : "Disconnected"}</p>
+        {role ? <p>{role.name}</p> : <></>}
       </main>
     </>
   );
--- src/server/ws.ts
+++ src/server/ws.ts
@@ -1,10 +1,14 @@
 import { Server } from "http";
 import { Server as SocketIOServer } from "socket.io";
+import { getCharacter } from "../util/characters";
 
 export default function ws(server: Server) {
   const io = new SocketIOServer(server);
   io.on("connection", (socket) => {
     console.log(`++ connect ${socket.id} (${io.engine.clientsCount})`);
+    socket.on("ready", () => {
+      socket.emit("role", getCharacter("drunk"));
+    });
     socket.on("disconnect", (reason) => {
       console.log(
         `-- disconnect ${socket.id} due to ${reason} (${io.engine.clientsCount})`
