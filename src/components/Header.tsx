9957b9094db1b57835f03eef61908e59315e628f
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-20 04:20:18 +0300

    Replace Header text with locale change link.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useTranslation } from "next-i18next";
+
 const Header = () => {
+  const router = useRouter();
+  const { t } = useTranslation();
   return (
-    <div className="flex-1 flex-grow-0 text-center font-serif text-2xl">
-      Clocktower App
+    <div className="flex flex-1 flex-grow-0 flex-row justify-items-center text-center align-middle font-serif">
+      <Link href={router.asPath} locale={router.locale === "en" ? "fi" : "en"}>
+        <div className="m-auto mr-0 rounded-lg p-3 font-sans text-lg shadow-sm shadow-white hover:cursor-pointer">
+          {t("header.changeLanguage")}
+        </div>
+      </Link>
     </div>
   );
 };
