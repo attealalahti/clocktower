cead94ecbce5af1a34b3b226e30652e12b58f5f4
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-21 13:20:41 +0300

    Add hover styles to language switch button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <div className="flex flex-1 flex-grow-0 flex-row justify-items-center text-center align-middle font-serif">
       <Link href={router.asPath} locale={router.locale === "en" ? "fi" : "en"}>
-        <div className="m-auto mr-0 rounded-lg border border-white p-3 font-sans text-lg hover:cursor-pointer">
+        <div className="m-auto mr-0 rounded-lg border border-white p-3 font-sans text-lg hover:cursor-pointer hover:bg-white hover:text-black">
           {t("header.changeLanguage")}
         </div>
       </Link>
