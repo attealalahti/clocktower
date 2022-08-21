fb76a22cecdeadc83388a854a0de6cced6bb9570
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-22 02:24:33 +0300

    Add prompt to enter name when signing in from home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,25 @@ import i18n from "../../next-i18next.config.mjs";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router.js";
 import Header from "../components/Header";
+import { useState } from "react";
+import loadingAnimation from "../../public/images/loading.svg";
+import Image from "next/image.js";
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
   const router = useRouter();
   const { data: session } = useSession();
+  const [signing, setSigning] = useState<boolean>(false);
 
   const joinGame = () => {
     if (session) {
       router.push("/play");
     } else {
-      signIn(undefined, { callbackUrl: `/${router.locale}/play` });
+      const name = prompt(t("enterName", ""));
+      if (name && name !== "") {
+        setSigning(true);
+        signIn("credentials", { name, callbackUrl: `/${router.locale}/play` });
+      }
     }
   };
 
@@ -23,21 +31,27 @@ const Home: NextPage = () => {
     <>
       <Header />
       <main className="min-w-screen flex flex-auto flex-col items-center justify-center gap-20 text-center">
-        <h1 className="mt-5 font-serif text-4xl">Clocktower App</h1>
-        <button
-          onClick={joinGame}
-          className="flex h-40 w-60 items-center justify-center rounded-xl bg-red-800 p-3 text-3xl font-bold leading-relaxed shadow-xl ring-1 ring-white hover:cursor-pointer hover:shadow-gray-800 active:bg-red-900"
-        >
-          <span>{t("joinGame")}</span>
-        </button>
-        <a
-          href="https://wiki.bloodontheclocktower.com/Trouble_Brewing"
-          target="_blank"
-          rel="noreferrer"
-          className="mb-5 font-serif text-xl underline underline-offset-2"
-        >
-          Blood on the Clocktower Wiki
-        </a>
+        {signing ? (
+          <Image src={loadingAnimation} alt="loading" />
+        ) : (
+          <>
+            <h1 className="mt-5 font-serif text-4xl">Clocktower App</h1>
+            <button
+              onClick={joinGame}
+              className="flex h-40 w-60 items-center justify-center rounded-xl bg-red-800 p-3 text-3xl font-bold leading-relaxed shadow-xl ring-1 ring-white hover:cursor-pointer hover:shadow-gray-800 active:bg-red-900"
+            >
+              <span>{t("joinGame")}</span>
+            </button>
+            <a
+              href="https://wiki.bloodontheclocktower.com/Trouble_Brewing"
+              target="_blank"
+              rel="noreferrer"
+              className="mb-5 font-serif text-xl underline underline-offset-2"
+            >
+              Blood on the Clocktower Wiki
+            </a>
+          </>
+        )}
       </main>
     </>
   );
