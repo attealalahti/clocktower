48cf23b57f543eb84dcb83e032a9a77637f17549
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-22 15:48:25 +0300

    Remove spinner from singing in from home page and ignore later sign-ins.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,7 @@ import i18n from "../../next-i18next.config.mjs";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router.js";
 import Header from "../components/Header";
-import { useEffect, useState } from "react";
-import loadingAnimation from "../../public/images/loading.svg";
-import Image from "next/image.js";
+import { useState } from "react";
 
 const Home: NextPage = () => {
   const { t } = useTranslation();
@@ -20,42 +18,32 @@ const Home: NextPage = () => {
       router.push("/play");
     } else {
       const name = prompt(t("enterName", ""));
-      if (name && name !== "") {
+      if (name && name !== "" && !signing) {
         setSigning(true);
         signIn("credentials", { name, callbackUrl: `/${router.locale}/play` });
       }
     }
   };
 
-  useEffect(() => {
-    setSigning(false);
-  }, []);
-
   return (
     <>
       <Header />
       <main className="min-w-screen flex flex-auto flex-col items-center justify-center gap-20 text-center">
-        {signing ? (
-          <Image src={loadingAnimation} alt="loading" />
-        ) : (
-          <>
-            <h1 className="mt-5 font-serif text-4xl">Clocktower App</h1>
-            <button
-              onClick={joinGame}
-              className="flex h-40 w-60 items-center justify-center rounded-xl bg-red-800 p-3 text-3xl font-bold leading-relaxed shadow-xl ring-1 ring-white hover:cursor-pointer hover:shadow-gray-800 active:bg-red-900"
-            >
-              <span>{t("joinGame")}</span>
-            </button>
-            <a
-              href="https://wiki.bloodontheclocktower.com/Trouble_Brewing"
-              target="_blank"
-              rel="noreferrer"
-              className="mb-5 font-serif text-xl underline underline-offset-2"
-            >
-              Blood on the Clocktower Wiki
-            </a>
-          </>
-        )}
+        <h1 className="mt-5 font-serif text-4xl">Clocktower App</h1>
+        <button
+          onClick={joinGame}
+          className="flex h-40 w-60 items-center justify-center rounded-xl bg-red-800 p-3 text-3xl font-bold leading-relaxed shadow-xl ring-1 ring-white hover:cursor-pointer hover:shadow-gray-800 active:bg-red-900"
+        >
+          <span>{t("joinGame")}</span>
+        </button>
+        <a
+          href="https://wiki.bloodontheclocktower.com/Trouble_Brewing"
+          target="_blank"
+          rel="noreferrer"
+          className="mb-5 font-serif text-xl underline underline-offset-2"
+        >
+          Blood on the Clocktower Wiki
+        </a>
       </main>
     </>
   );
