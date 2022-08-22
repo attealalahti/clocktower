5b8b88630e421e53ef6bdb47fd7c2eb06d629eae
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-23 01:48:52 +0300

    Add night order section to pages/cheatsheet.

--- src/pages/cheatsheet.tsx
+++ src/pages/cheatsheet.tsx
@@ -6,10 +6,26 @@ import i18n from "../../next-i18next.config.mjs";
 import Header from "../components/Header";
 import { characters as characterMap } from "../util/characters";
 
-const CheatSheet: NextPage = () => {
-  characterMap.delete("unassigned");
-  const characters = Array.from(characterMap);
+characterMap.delete("unassigned");
+const characters = Array.from(characterMap);
+const firstNightChars = characters.filter(
+  ([, char]) => char.firstNightOrder !== undefined
+);
+firstNightChars.sort(([, charA], [, charB]) => {
+  charA.firstNightOrder = charA.firstNightOrder as number;
+  charB.firstNightOrder = charB.firstNightOrder as number;
+  return charA.firstNightOrder - charB.firstNightOrder;
+});
+const otherNightChars = characters.filter(
+  ([, char]) => char.nightOrder !== undefined
+);
+otherNightChars.sort(([, charA], [, charB]) => {
+  charA.nightOrder = charA.nightOrder as number;
+  charB.nightOrder = charB.nightOrder as number;
+  return charA.nightOrder - charB.nightOrder;
+});
 
+const CheatSheet: NextPage = () => {
   const { t } = useTranslation();
   return (
     <>
@@ -38,13 +54,61 @@ const CheatSheet: NextPage = () => {
             </div>
           </div>
         ))}
-        <h2 className="font-serif text-xl">{t("cs.nightOrder")}</h2>
-        <div>
-          <div>
-            <h3>{t("cs.firstNight")}</h3>
+        <h2 className="my-3 font-serif text-xl">{t("cs.nightOrder")}</h2>
+        <div className="flex flex-row flex-wrap gap-5">
+          <div className="mx-auto">
+            <h3 className="font-serif">{t("cs.firstNight")}</h3>
+            <ol className="my-2 rounded-lg p-2 shadow-sm shadow-white">
+              {firstNightChars.map(([id], index) => (
+                <li
+                  key={id}
+                  className="flex flex-row justify-center gap-3 text-left align-middle font-serif"
+                >
+                  <div className="m-auto flex-1 flex-grow-0">{`${
+                    index + 1
+                  }.`}</div>
+                  <div className="m-auto flex-auto">
+                    {t(`characters.${id}.name`)}
+                  </div>
+                  <div className="flex-3 m-auto">
+                    <Image
+                      src={`/images/${id}.webp`}
+                      alt={t(`characters.${id}.name`)}
+                      width={59}
+                      height={41}
+                      layout={"fixed"}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ol>
           </div>
-          <div>
-            <h3>{t("cs.otherNights")}</h3>
+          <div className="mx-auto">
+            <h3 className="font-serif">{t("cs.otherNights")}</h3>
+            <ol className="my-2 rounded-lg p-2 shadow-sm shadow-white">
+              {otherNightChars.map(([id], index) => (
+                <li
+                  key={id}
+                  className="flex flex-row justify-center gap-3 text-left align-middle font-serif"
+                >
+                  <div className="m-auto flex-1 flex-grow-0">{`${
+                    index + 1
+                  }.`}</div>
+                  <div className="m-auto flex-auto">
+                    {t(`characters.${id}.name`)}
+                  </div>
+                  <div className="flex-3 m-auto">
+                    <Image
+                      src={`/images/${id}.webp`}
+                      alt={t(`characters.${id}.name`)}
+                      width={59}
+                      height={41}
+                      layout={"fixed"}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ol>
           </div>
         </div>
       </main>
