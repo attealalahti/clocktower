6c2127438a94bdf95265c3bc132d61cfa4a56115
Atte Ala-Lahti <atte.ala-lahti@tuni.fi>
2022-08-25 21:25:09 +0300

    Modify modal size to better fit on mobile screens.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,7 +8,7 @@ const Modal = ({ open, children }: ModalProps) => {
   return createPortal(
     <>
       <div className="fixed top-0 bottom-0 left-0 right-0 bg-black opacity-70" />
-      <div className="fixed top-2/4 left-2/4 h-screen w-screen -translate-y-1/2 -translate-x-1/2 p-5">
+      <div className="fixed top-2/4 left-2/4 h-screen w-screen -translate-y-1/2 -translate-x-1/2 p-5 py-10">
         {children}
       </div>
     </>,
